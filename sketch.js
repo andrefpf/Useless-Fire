091f51b993270de3d5680185a126fab4ac5199fe
André Filipe da Silva Fernandes <fpf.andre@gmail.com>
2019-03-08 01:21:30 -0300

    tem um grid que obedece um array de cores e consegue copiar a cor das células debaixo. Tem um bug que só deixa ele fazer isso uma vez

--- sketch.js
+++ sketch.js
@@ -2,40 +2,55 @@ function setup(){
   createCanvas(400,400);
   scale = 5;
   cellSize = width/scale;
+  collorList = [];
+  collorListTemp = [];
+
 
 }
 
 function draw(){
   background(100);
 
-  // for(i=0;i<scale;i++){
-  //   for(j=0;j<scale;j++){
-  //     cells(i,j);
-  //   }
-  // }
+  quadradosAbaixo = 2;
 
   for(i=1;i<=scale*scale;i++){
     cells(i);
   }
+  collorList = collorListTemp;
+  collorListTemp = [];
 }
 
 function cells(cellNumber){
 
-  x = cellNumber%scale;
+  x = (cellNumber-1)%scale;
   y = ceil(cellNumber/scale)-1;
 
 
   push();
 
-  fill(255);
-  cellCollor();
+  fill(0);
+  cellCollor(cellNumber);
   stroke(0);
   strokeWeight(2);
   rect(x*cellSize,y*cellSize,cellSize,cellSize);
 
   pop();
+  text(cellNumber, x*cellSize+cellSize/2, y*cellSize+cellSize/2);
 }
 
-function cellCollor(x,y){
+function cellCollor(cellNumber){
+
+  cor = collorList[cellNumber-1+(scale*quadradosAbaixo)]+40;
+  if(cellNumber > scale*scale-scale){
+    cor = 10;
+  }
+
+
+  fill(cor*2,cor,0);
+  collorListTemp.push(cor);
+
+  //cor = collorList[cellNumber-1]
+
+
 
 }
