77cd590b76dc493736bd1c5a8094728cd03dd7e3
André Filipe da Silva Fernandes <fpf.andre@gmail.com>
2019-03-07 22:07:03 -0300

    criei um grid simples

--- sketch.js
+++ sketch.js
@@ -1,17 +1,25 @@
-scl = 0;
-cell = [];  
-
 function setup(){
-  createCanvas(400,400);  
+  createCanvas(400,400);
+  cellSize = width/5;
 }
 
 function draw(){
-  background(0);
+  background(100);
 
-  for(i=0; i<width/scl; i++){
-    for(j=0;j<=height/scl; j++){
-      rect(i,j,scl,scl)
+  for(i=0;i<cellSize;i++){
+    for(j=0;j<cellSize;j++){
+      cells(i,j);
     }
   }
-  
+}
+
+function cells(xPosition, yPosition){
+  push();
+
+  fill(200);
+  stroke(0);
+  strokeWeight(2);
+  rect(xPosition*cellSize,yPosition*cellSize,cellSize,cellSize);
+
+  pop();
 }
