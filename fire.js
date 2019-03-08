d981bb76f9d426cc4792a195491403972582e795
André Filipe da Silva Fernandes <fpf.andre@gmail.com>
2019-03-08 02:23:44 -0300

    coloquei todos os arquivos necessários dentro da pasta, mudei o nome do arquivo pra fire pra upar bonitinho pro github

---
+++ fire.js
@@ -0,0 +1,58 @@
+function setup(){
+  createCanvas(400,400);
+  scale = 50;
+  cellSize = width/scale;
+  collorList = [];
+  collorListTemp = [];
+
+  frameRate(12);
+
+}
+
+function draw(){
+  background(100);
+
+  for(i=1;i<=scale*scale;i++){
+    cells(i);
+  }
+  collorList = collorListTemp;
+  collorListTemp = [];
+}
+
+function cells(cellNumber){
+
+  x = (cellNumber-1)%scale;
+  y = ceil(cellNumber/scale)-1;
+
+
+  push();
+
+  fill(0);
+  cellCollor(cellNumber);
+  noStroke();
+  //strokeWeight(1);
+  rect(x*cellSize,y*cellSize,cellSize,cellSize);
+
+  pop();
+  //text(cellNumber, x*cellSize+cellSize/2, y*cellSize+cellSize/2);
+}
+
+function cellCollor(cellNumber){
+
+  quadradosAbaixo = floor(random(1,4));
+
+  cor = collorList[cellNumber-1+(scale*quadradosAbaixo)+quadradosAbaixo]-20;
+
+  if(cellNumber > scale*scale-scale*4){
+    cor = 150;
+  }
+
+
+  fill(cor*2,cor,0);
+  collorListTemp.push(cor);
+
+  //cor = collorList[cellNumber-1]
+
+
+
+}
